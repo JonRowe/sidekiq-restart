06d1515b518b037653c03dba226a7fcfbbdeedb7
Jon Rowe <hello@jonrowe.co.uk>
2014-01-10 10:51:15 +1100

    handle no key present

--- lib/sidekiq/restart/command.rb
+++ lib/sidekiq/restart/command.rb
@@ -9,13 +9,15 @@ module Sidekiq
       def run id
         @sidekiq.redis do |conn|
           payload = conn.get("worker:#{id}")
-          msg = @sidekiq.load_json(payload)[:payload]
-          Sidekiq::Client.push msg
+          if payload
+            msg = @sidekiq.load_json(payload)[:payload]
+            Sidekiq::Client.push msg
 
-          # cleanup redis details about the worker
-          conn.srem("workers", id)
-          conn.del("worker:#{id}")
-          conn.del("worker:#{id}:started")
+            # cleanup redis details about the worker
+            conn.srem("workers", id)
+            conn.del("worker:#{id}")
+            conn.del("worker:#{id}:started")
+          end
         end
       end
 
